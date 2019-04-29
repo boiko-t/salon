c5baeb6ac2d5e261e18059544f6b49ecd2500144
Taisa Boiko <tayaboyko@gmail.com>
2019-04-29 20:30:47 +0300

    update(Home): added menu

--- app/src/main/java/com/boiko/taisa/salon/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/boiko/taisa/salon/ui/activity/HomeActivity.kt
@@ -1,14 +1,22 @@
 package com.boiko.taisa.salon.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import com.boiko.taisa.salon.BuildConfig
 import com.boiko.taisa.salon.R
 import com.boiko.taisa.salon.domain.entity.Category
 import com.boiko.taisa.salon.mvp.home.Home
 import com.boiko.taisa.salon.mvp.home.HomePresenter
 import com.boiko.taisa.salon.ui.recyclerview.CategoryRecyclerViewAdapter
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
 
 class HomeActivity : AppCompatActivity(), Home.View {
     private lateinit var presenter: HomePresenter
@@ -19,6 +27,7 @@ class HomeActivity : AppCompatActivity(), Home.View {
         setContentView(R.layout.activity_home)
         presenter = HomePresenter()
         findViews()
+        configureMenu()
     }
 
     override fun onStart() {
@@ -41,5 +50,36 @@ class HomeActivity : AppCompatActivity(), Home.View {
 
     private fun findViews() {
         categoryRecyclerView = findViewById(R.id.rvCategoriesCollection)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_sign_out -> {
+            FirebaseAuth.getInstance().signOut()
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(BuildConfig.GOOGLE_REQUEST_ID_TOKEN)
+                    .requestEmail()
+                    .build()
+
+            val googleSignInClient = GoogleSignIn.getClient(this, gso)
+            googleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun configureMenu() {
+//        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
--- app/src/main/java/com/boiko/taisa/salon/ui/recyclerview/CategoryRecyclerViewAdapter.java
+++ app/src/main/java/com/boiko/taisa/salon/ui/recyclerview/CategoryRecyclerViewAdapter.java
@@ -1,7 +1,5 @@
 package com.boiko.taisa.salon.ui.recyclerview;
 
-import android.graphics.drawable.PictureDrawable;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +10,8 @@ import android.widget.TextView;
 
 import com.boiko.taisa.salon.R;
 import com.boiko.taisa.salon.domain.entity.Category;
-import com.bumptech.glide.GenericRequestBuilder;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.model.StreamEncoder;
 import com.squareup.picasso.Picasso;
 
-import java.io.InputStream;
 import java.util.List;
 
 public class CategoryRecyclerViewAdapter extends RecyclerView.Adapter<CategoryRecyclerViewAdapter.ViewHolder> {
